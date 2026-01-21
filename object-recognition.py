dde32b5d718d76b6f00dc89c6e2c4b2b873e4dff
Nathan Ha <nathanha562@gmail.com>
2026-01-20 23:07:15 -0800

    Changed some words

--- object-recognition.py
+++ object-recognition.py
@@ -2,9 +2,12 @@
 
 from ultralytics import YOLO
 import cv2
+import time
 
+# model
 model = YOLO("yolov8n.pt")
 
+# open camera (0 = default camera)
 cap = cv2.VideoCapture(0)
 
 if not cap.isOpened():
@@ -19,12 +22,13 @@ while True:
 
     annotated_frame = results[0].plot()
 
-    cv2.imshow("balls", annotated_frame)
+    cv2.imshow("Object Detection", annotated_frame)
 
+    print("Hold 'q' on the window to exit.")
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     
-    print("Hold 'q' on the window to exit.")
+    # time.sleep(0.5)
 
 cap.release()
 cv2.destroyAllWindows()
