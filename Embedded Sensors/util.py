7f88101d01ccd15b542ea571050131d6f83736fa
mcuaya1 <mcuay001@ucr.edu>
2026-02-17 16:05:08 -0800

    Reverting to old util code

--- Embedded Sensors/util.py
+++ Embedded Sensors/util.py
@@ -1,2 +1,2 @@
 def map_range(x, in_min, in_max, out_min, out_max):
-    return int(out_min + (float(x - in_min)/float(in_max-in_min) * (out_max) - out_min))
+  return (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
