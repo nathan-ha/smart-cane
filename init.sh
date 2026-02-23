ae62948f389b3ce8ae556606c382dbe7afeeeb9d
mcuaya1 <mcuay001@ucr.edu>
2026-02-22 18:28:45 -0800

    Updating environment setup

--- init.sh
+++ init.sh
@@ -2,13 +2,12 @@
 
 echo "Setting up environment and installing packages..."
 
-python3 -m venv .venv
+python3 -m venv --system-site-packages test_venv
 
-source .venv/bin/activate
+source test_venv/bin/activate
 
 pip install -r requirements.txt --no-cache-dir --only-binary :all:
 
 deactivate
 
 echo "Finished..."
-
