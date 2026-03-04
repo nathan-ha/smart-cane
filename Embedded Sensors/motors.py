8e10fe821ea6fc1d61e46745e6a7a63ea0c4d399
Mario Cuaya <80364641+mcuaya1@users.noreply.github.com>
2026-03-03 18:10:01 -0800

    Update motors.py

--- Embedded Sensors/motors.py
+++ Embedded Sensors/motors.py
@@ -4,6 +4,7 @@ PWM_MAX = 32767
 PWM_MIN = 1
 
 MOTORS = {
-  "left" : PWM(Pin(6), freq=50, duty_u16=0),
-  "right" : PWM(Pin(7), freq=50, duty_u16=0),
+  "left" : PWM(Pin(16), freq=50, duty_u16=0),
+  "right" : PWM(Pin(17), freq=50, duty_u16=0),
+  "middle": PWM(Pin(18)), freq=50, duty_u16=0),
 }
